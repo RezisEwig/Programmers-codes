d5893392a9388e05939ffef7a5838a579653e554
RezisEwig <tkdidls1@naver.com>
2021-04-13 20:15:42 +0900

    8일차 업로드

---
+++ Level_1/K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    new_array = []
+    
+    for c in commands :
+        new_array = array[c[0]-1:c[1]]
+        new_array.sort()
+        answer.append(new_array[c[2]-1])
+        
+    return answer
